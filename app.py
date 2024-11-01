1bdac129df917b82ecd755138ce828b3bbd62588
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2024-11-01 15:32:35 -0400

    done with tests

--- app.py
+++ app.py
@@ -17,56 +17,87 @@ def homepage():
 @app.route('/froyo')
 def choose_froyo():
     """Shows a form to collect the user's Fro-Yo order."""
-    pass
+    return render_template("froyo_form.html")
 
 @app.route('/froyo_results')
 def show_froyo_results():
-    """Shows the user what they ordered from the previous page."""
-    pass
+    context = {
+        'users_froyo_flavor' : request.args.get('flavor'),
+        'users_froyo_toppings' : request.args.get('toppings')
+    }
+    return render_template('froyo_results.html', **context)
 
 @app.route('/favorites')
 def favorites():
     """Shows the user a form to choose their favorite color, animal, and city."""
-    pass
+    return """
+    <form action="/favorites_results" method="GET">
+        What is your favorite color?
+        <input type="text" name="color">
+        What is your favorite animal?
+        <input type="text" name="animal">
+        What is your favorite city?
+        <input type="text" name="city">   
+        <input type="submit" value="Submit!">         
+    </form>
+    """
 
 @app.route('/favorites_results')
 def favorites_results():
     """Shows the user a nice message using their form results."""
-    pass
+    favorite_color = request.args.get("color")
+    favorite_animal = request.args.get("animal")
+    favorite_city = request.args.get("city")
+    return f"Wow, I didn't know {favorite_color} {favorite_animal} lived in {favorite_city}!"
 
 @app.route('/secret_message')
 def secret_message():
     """Shows the user a form to collect a secret message. Sends the result via
     the POST method to keep it a secret!"""
-    pass
-
-@app.route('/message_results', methods=['POST'])
-def message_results():
-    """Shows the user their message, with the letters in sorted order."""
-    pass
-
-@app.route('/calculator')
-def calculator():
-    """Shows the user a form to enter 2 numbers and an operation."""
     return """
-    <form action="/calculator_results" method="GET">
-        Please enter 2 numbers and select an operator.<br/><br/>
-        <input type="number" name="operand1">
-        <select name="operation">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="operand2">
+    <form action="/message_results" method="POST">
+        Enter a secret message
+        <input type="text" name="message">
         <input type="submit" value="Submit!">
     </form>
     """
 
+@app.route('/message_results', methods=['POST'])
+def message_results():
+    """Shows the user their message, with the letters in sorted order."""
+    message = request.form.get("message")
+    message = sort_letters(message)
+    return f"""Here's your secret message!\n{message}"""
+
+@app.route('/calculator')
+def calculator():
+    """Shows the user a form to enter 2 numbers and an operation."""
+    return render_template('calculator_form.html')
+
 @app.route('/calculator_results')
 def calculator_results():
     """Shows the user the result of their calculation."""
-    pass
+    operand1 = request.args.get("operand1")
+    operand2 = request.args.get("operand2")
+    operation = request.args.get("operation")
+    if operation == "add":
+        result = int(operand1) + int(operand2)
+    elif operation == "substract":
+        result = int(operand1) - int(operand2)
+    elif operation == "multiply":
+        result = int(operand1) * int(operand2)
+    elif operation == "divide":
+        result = int(operand1) / int(operand2)
+    else:
+        result = "Error: Unknown operation"
+
+    context = {
+        'operand1': request.args.get("operand1"),
+    'operation': request.args.get("operation"),
+    'operand2': request.args.get("operand2"),
+    'result': str(result)
+    }
+    return render_template('calculator_results.html', **context) 
 
 
 HOROSCOPE_PERSONALITIES = {
@@ -93,17 +124,19 @@ def horoscope_form():
 def horoscope_results():
     """Shows the user the result for their chosen horoscope."""
 
+    name = request.args.get('users_name')
     # TODO: Get the sign the user entered in the form, based on their birthday
-    horoscope_sign = ''
+    horoscope_sign = request.args.get('horoscope_sign')
 
     # TODO: Look up the user's personality in the HOROSCOPE_PERSONALITIES
     # dictionary based on what the user entered
-    users_personality = ''
+    users_personality = HOROSCOPE_PERSONALITIES[horoscope_sign]
 
     # TODO: Generate a random number from 1 to 99
-    lucky_number = 0
+    lucky_number = random.randint(1, 99)
 
     context = {
+        'name': name,
         'horoscope_sign': horoscope_sign,
         'personality': users_personality, 
         'lucky_number': lucky_number
